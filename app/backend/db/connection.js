9f350591b32f10aa6aa9513995eb3e9bd19f383b
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2024-05-07 11:50:40 +0200

    Update connection.js

--- app/backend/db/connection.js
+++ app/backend/db/connection.js
@@ -8,7 +8,7 @@ const db = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
+    port: process.env.MYSQL_DOCKER_PORT
 });
 
 
