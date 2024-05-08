70d0dfc68f20399e3c9797f9a7b28dafe64c109a
Bartek Dyk <debartek19@hotmail.com>
2024-05-08 20:25:19 +0200

    Removed login token from client console, commented logging token in backend console

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -15,7 +15,6 @@ const App = () => {
   const authenticate = async () => {
     try {
       const token = localStorage.getItem('token');
-      console.log(token)
       if (token) {
         setLoggedIn(true)
       } else {
