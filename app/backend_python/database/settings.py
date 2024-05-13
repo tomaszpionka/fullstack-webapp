71f91332bec44154410807877bb4f959f0a2b339
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2024-05-13 12:16:57 +0200

    Removed unused var

--- app/backend_python/database/settings.py
+++ app/backend_python/database/settings.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
-MYSQL_ROOT_PASSWORD = os.environ.get("MYSQL_ROOT_PASSWORD")
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
 MYSQL_USER = os.environ.get("MYSQL_USER")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
